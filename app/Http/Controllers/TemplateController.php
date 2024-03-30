51e93c17eebd9682a7152157459e93eb8115d19a
Kunal <kunal98v@gmail.com>
2024-03-30 18:32:51 +0530

    third

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Throwable;
+use App\Models\DataType;
 use App\Models\Template;
+use App\Models\Validation;
 use Illuminate\Http\Request;
 use App\Models\TemplateField;
-use Exception;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -67,48 +69,70 @@ public function update(Request $request){
     $validator = Validator::make($request->all(), $rules);
     if ($validator->fails() ) {
         return response()->json(['status' => false, 'errors' => $validator->errors()]);
-    }      
+    }
+    $fieldsFromRequest = collect ($request->fields);
+    $fieldIds = $fieldsFromRequest?->pluck('field_id')->toArray();
+
+    TemplateField::where('template_id', $request->id)->whereNotIn('id', $fieldIds)->delete();
     $record = Template::with('template_fields')->find($request->id);
     $record->template_name =  $request->template_name;
     $record->save();
-    $arr = $request->fields;
-    foreach ($arr as $data) {
-        $field = TemplateField::find($data["field_id"]);
+
+    // foreach($record_arr as $val){
+    //     foreach ($arr as $data) {
+    //         if ($data["field_id"] != $val["id"]){
+    //             $val->delete();            
+    //         }  
+    //     }
+    // }
+
+    foreach ($request->fields as $data) {
+        $field = TemplateField::find($data['field_id']);
+
         if (!$field) {
             $field = new TemplateField();
-            $field->template_id = $record->id; 
+            $field->template_id = $request->template_id;
+            $field->id = $data["field_id"];
         }
+        
+        if (TemplateField::find($data["field_name"])){
             $field->field_name = $data["field_name"];
-            $field->data_type_id = $data["data_type"];
-            $field->is_mandatory = $data["is_mandatory"];
-            $field->validation_id = $data["validations"];
-            $field->template_id = $record->id; 
-            // $record->template_fields()->save($field);
-            $field->save();
-    }
-}catch (Throwable $e) {
-    Log::error($e->getMessage(), [$e->getTraceAsString()]);
-    throw new HttpResponseException(response()->json(['status' => false , 'message' => 'Something Went Wrong', 'errors' => $e->getTraceAsString()]));
-}
-    return "Data updated !";
-}
+        } else {
+            return "Field name already exists !";
+        }
 
+        if (DataType::find($data["data_type"])){
+            $field->data_type_id = $data["data_type"];
+        } else {
+            return "Data Type Not Found !";
+        }
+
+        $field->is_mandatory = $data["is_mandatory"];
+
+        if (Validation::find($data["validations"])){
+            $field->validation_id = $data["validations"];
+        } else {
+            return "Validation id Not Found !";
+        }
+        $field->template_id = $request->id; 
+        $field->save();
+    }
+} catch (Throwable $e) {
+    Log::error($e->getMessage(), [$e->getTraceAsString()]);
+    throw new HttpResponseException(response()->json(['status' => false , 'message' => $e->getMessage(), 'errors' => $e->getTraceAsString()]));
+}
+    return "updated !" ;
+}
 
 
     public function delete(Request $request){
         $template_record = Template::find($request->template_id);
         //$record = TemplateField::find($request->field_id);
-        if($template_record){
-            try{
-            $template_record->delete();
-
-            }catch(Exception){
-                return "Cannot delete template before deleting the fields!";
-            }
+        if ($template_record){
             return "Deleted";
-        }
-        else
+        } else{
             return "No template Found";
+        }
     }
 }
 
